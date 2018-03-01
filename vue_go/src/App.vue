4bb62bb4c4b2c88f040937fbb8c998eb2cd29d2b
Unknown <radhouen.assakra@satoripop.com>
2018-03-01 17:00:38 +0100

    Update users list display

--- vue_go/src/App.vue
+++ vue_go/src/App.vue
@@ -1,13 +1,37 @@
 <template>
-    <div id="app">
+    <div id="app" class="container">
+            
+            
+            <br>
+        <div v-if="users && users.length" class="row">
+            
+            <div class="col-4" v-for="user of users">
+                      <div  class="card testimonial-card" >
 
-        <ul v-if="posts && posts.length">
-            <li v-for="post of posts">
-                <p><strong>{{post.Name}}</strong></p>
-                <p><strong>{{post.Email}}</strong></p>
+        <!--Bacground color-->
+        <div class="card-up indigo lighten-1">
+        </div>
 
-            </li>
-        </ul>
+        <!--Avatar-->
+        <div class="avatar mx-auto white"><img src="https://mdbootstrap.com/img/Photos/Avatars/img%20%2810%29.jpg" class="rounded-circle">
+        </div>
+
+        <div class="card-body">
+            <!--Name-->
+            <h4 class="card-title">{{user.Name}}</h4>
+            <hr>
+            <!--Quotation-->
+            <p><i class="fa fa-quote-left"></i> {{user.Email}}</p>
+        </div>
+
+    </div>
+    <!--/.Card-->
+
+            </div>
+                <!--Card-->
+
+        </div>
+                                
 
         <ul v-if="errors && errors.length">
             <li v-for="error of errors">
@@ -19,29 +43,28 @@
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from "axios";
 
-    export default {
-        data() {
-            return {
-                posts: [],
-                errors: []
-            }
-        },
+export default {
+  data() {
+    return {
+      users: [],
+      errors: []
+    };
+  },
 
-        // Fetches posts when the component is created.
-        created() {
-            axios.get(`http://localhost:10000/users`)
-                .then(response => {
-                // JSON responses are automatically parsed.
-                this.posts = response.data
-                    console.log(this.posts);
-        })
-        .
-            catch(e => {
-                this.errors.push(e)
-        })
-
-        }
-    }
+  // Fetches posts when the component is created.
+  created() {
+    axios
+      .get(`http://localhost:10000/users`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.users = response.data;
+        console.log(this.users);
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
+  }
+};
 </script>
